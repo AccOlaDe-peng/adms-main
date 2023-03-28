33c162eb1858af32af248fff2e84573678e0ab80
pengrenchang <pengrch@activeio.com.cn>
2023-03-28 18:35:07 +0800

    fix: are

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <App />
-);
+if (window.__POWERED_BY_WUJIE__) {
+    window.__WUJIE_MOUNT = () => {
+        ReactDOM.createRoot(
+            document.getElementById("root") as HTMLElement
+        ).render(<App />);
+    };
+    window.__WUJIE_UNMOUNT = () => {
+        ReactDOM.createRoot(
+            document.getElementById("root") as HTMLElement
+        ).unmount();
+    };
+    window.__WUJIE.mount();
+} else {
+    ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+        <App />
+    );
+}
